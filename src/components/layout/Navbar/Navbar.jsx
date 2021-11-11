1e29073d7800fc756eac8fa46f18b1005f4d9c1d
Trinidad Padilla <tpadilla101@live.com>
2021-11-10 19:52:46 -0800

    finished basic react-scroll

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -1,16 +1,38 @@
    /* THIS IS THE COMPONENT FOR THE NAVBAR: */
 
-    import React from 'react'; //Keyboard VS Code Shortcut: rafce
+    import React, {useState, useEffect} from 'react'; //Keyboard VS Code Shortcut: rafce
     import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './NavbarStyles.js';
     import {FaBars} from 'react-icons/fa';
     import { navData } from '../../../utils/seed';
+    import { animateScroll as scroll } from 'react-scroll';
 
     const Navbar = ( {toggle, isOpen } ) => {
+
+    /* State for... */
+        const [ scrollNav, setScrollNav ] = useState(false);
+
+        const changeNav = () => {
+            if( window.scrollY >= 80) {
+                setScrollNav(true);
+            } else {
+                setScrollNav(false);
+            }
+        };
+
+    /* Looks for 'scroll' event */
+        useEffect( () => {
+            window.addEventListener('scroll', changeNav)
+        }, []);
+
+        const toggleHome = () => {
+            scroll.scrollToTop();
+        };
+
         return (
             <>
-                <Nav>
+                <Nav scrollNav={scrollNav}>
                     <NavbarContainer>
-                        <NavLogo to='/'>
+                        <NavLogo to='/' onClick={toggleHome}>
                             Template
                         </NavLogo>
                         <MobileIcon onClick={toggle}>
@@ -21,7 +43,7 @@
                             {navData.map( (data) => {
                                 return (
                                     <NavItem key={data.id}>
-                                        <NavLinks to={data.path}>
+                                        <NavLinks to={data.path} smooth={data.smooth} duration={data.duration} spy={data.spy} exact={data.exact} offset={data.offset}>
                                             {data.title}
                                         </NavLinks>
                                     </NavItem>
--- src/components/layout/Navbar/NavbarStyles.js
+++ src/components/layout/Navbar/NavbarStyles.js
@@ -5,7 +5,7 @@ import { Link as LinkReactRouter } from 'react-router-dom';
 import { Link as LinkScroll } from 'react-scroll';
 
 export const Nav = styled.nav`
-    background: #000;
+    background: ${ ( { scrollNav } ) => (scrollNav ? '#000' : 'transparent')};
     height: 80px;
     margin-top: -80px;
     display: flex;
