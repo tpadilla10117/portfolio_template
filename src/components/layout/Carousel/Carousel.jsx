d122bf28acd0ac0dd15457078d1d0993d893d5f1
Trinidad Padilla <tpadilla101@live.com>
2021-11-14 11:33:44 -0800

    Need to make carousel fully responsive

--- src/components/layout/Carousel/Carousel.jsx
+++ src/components/layout/Carousel/Carousel.jsx
@@ -3,6 +3,7 @@ import { carouselData } from '../../../utils/seed';
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
 import { 
     CarouselWrapper,
+    CarouselImgWrapper,
     CarouselImg
 } from './CarouselStyles';
 import './Carousel.css';
@@ -12,13 +13,32 @@ const Carousel = ( {slides} ) => {
     const [ current, setCurrent ] = useState(0);
     const length = slides.length;
 
+/* If no data, or our array isn't an array, return null: */
+    if(!Array.isArray(slides) || slides.length <= 0 ) {
+        return null;
+    }
+
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1);
+    };
+
+    const previousSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1);
+    }
+
+    console.log("Here is the current slide: ", current)
+
     return (
         <CarouselWrapper className="slider">
-            <FaArrowAltCircleLeft className="left-arrow"/>
-            <FaArrowAltCircleRight className="right-arrow"/>
+            <FaArrowAltCircleLeft className="left-arrow" onClick={previousSlide}/>
+            <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide}/>
             {carouselData.map( (slide, index) => {
                 return (
-                    <CarouselImg src={slide.image} alt="travel" key={index} className='slider-image' />
+                    <CarouselImgWrapper className={index === current ? 'slide active' : 'slide'} key={index} >
+                        {index === current && (<CarouselImg src={slide.image} alt="travel" key={index} className='slider-image' />) }
+                        
+                    </CarouselImgWrapper>
+                    
                 )
             })}
             
--- src/components/layout/Carousel/CarouselStyles.js
+++ src/components/layout/Carousel/CarouselStyles.js
@@ -4,12 +4,32 @@ export const CarouselWrapper = styled.section`
     display: flex;
     position: relative;
     height: 100%;
+    width: 100%;
     justify-content: center;
     /* align-items: center; */ /* this is making white space appear */
+
+`
+/* TODO: Need to make carousel fully responsive: */
+
+export const CarouselImgWrapper = styled.div`
+    height: 100vh;
+    /* width: 100%; */
+    /* flex-direction: column; */
+    /* justify-content: flex-start; */
+    /* align-items: center; */
+    /* border-radius: 10px; */
+    /* max-height: 340px; */
+   /*  padding: 30px; */
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+
 `
 
 export const CarouselImg = styled.img`
-    width: 1000px;
-    height: 600px;
+    max-width: 80%;
+    width: 100%;
+    object-fit: contain;
+    height: 400px;
     border-radius: 10px;
+    
 `
+
